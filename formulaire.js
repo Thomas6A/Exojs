dae6ce4ce8032c41c9ba4b12ef203157379ead62
Thomas6A <thomas.baudrin@outlook.fr>
2023-05-16 16:27:33 +0200

    exo

---
+++ formulaire.js
@@ -0,0 +1,9 @@
+let texte = document.querySelector('.texte')
+let email = document.querySelector('.email')
+let mdp = document.querySelector('.mdp')
+let envoyer = document.querySelector('.envoyer')
+let reset = document.querySelector('.reset')
+
+envoyer.addEventListener('click',function(){
+    if
+})
